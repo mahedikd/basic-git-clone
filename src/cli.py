0b2af835ff97567d7b732e57f657aa27758fb2fc
mahedikd <mahedi66958@gmail.com>
2025-07-15 11:02:05 +0600

    checkout: Read tree and move HEAD

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import textwrap
 
 import data
 import base
@@ -39,6 +40,14 @@ def parse_args():
     commit_parser.set_defaults(func=commit)
     commit_parser.add_argument("-m", "--message", required=True)
 
+    log_parser = commands.add_parser("log")
+    log_parser.set_defaults(func=log)
+    log_parser.add_argument("oid", nargs="?")
+
+    checkout_parser = commands.add_parser("checkout")
+    checkout_parser.set_defaults(func=checkout)
+    checkout_parser.add_argument("oid")
+
     return parser.parse_args()
 
 
@@ -67,3 +76,19 @@ def read_tree(args):
 
 def commit(args):
     print(base.commit(args.message))
+
+
+def log(args):
+    oid = args.oid or data.get_HEAD()
+    while oid:
+        commit = base.get_commit(oid)
+
+        print(f"commit {oid}\n")
+        print(textwrap.indent(commit.message, "    "))
+        print("")
+
+        oid = commit.parent
+
+
+def checkout(args):
+    base.checkout(args.oid)
