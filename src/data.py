dd3bb41829de33322288c6c301f44ceebcf74782
mahedikd <contact@solodevr.com>
2025-07-19 11:17:11 +0600

    push: Naive implementation

--- src/data.py
+++ src/data.py
@@ -2,6 +2,7 @@ import os
 import hashlib
 from collections import namedtuple
 from contextlib import contextmanager
+import shutil
 
 
 GIT_DIR = None
@@ -95,3 +96,19 @@ def get_object(oid, expected="blob"):
     if expected is not None:
         assert type_ == expected, f"Expected {expected}, got {type_}"
     return content
+
+
+def object_exists(oid):
+    return os.path.isfile(f"{GIT_DIR}/objects/{oid}")
+
+
+def fetch_object_if_missing(oid, remote_git_dir):
+    if object_exists(oid):
+        return
+    remote_git_dir += "/.ugit"
+    shutil.copy(f"{remote_git_dir}/objects/{oid}", f"{GIT_DIR}/objects/{oid}")
+
+
+def push_object(oid, remote_git_dir):
+    remote_git_dir += "/.ugit"
+    shutil.copy(f"{GIT_DIR}/objects/{oid}", f"{remote_git_dir}/objects/{oid}")
--- src/remote.py
+++ src/remote.py
@@ -1,10 +1,42 @@
+import os
+
 import data
+import base
+
+REMOTE_REFS_BASE = "refs/heads/"
+LOCAL_REFS_BASE = "refs/remote/"
 
 
 def fetch(remote_path):
-    print("Will fetch the following refs:")
-    for refname, _ in _get_remote_refs(remote_path, "refs/heads").items():
-        print(f"- {refname}")
+    # Get refs from server
+    refs = _get_remote_refs(remote_path, REMOTE_REFS_BASE)
+
+    # Fetch missing objects by iterating and fetching on demand
+    for oid in base.iter_objects_in_commits(refs.values()):
+        data.fetch_object_if_missing(oid, remote_path)
+
+    # Update local refs to match server
+    for remote_name, value in refs.items():
+        refname = os.path.relpath(remote_name, REMOTE_REFS_BASE)
+        data.update_ref(
+            f"{LOCAL_REFS_BASE}/{refname}", data.RefValue(symbolic=False, value=value)
+        )
+
+
+def push(remote_path, refname):
+    # Get refs data
+    local_ref = data.get_ref(refname).value
+    assert local_ref
+
+    objects_to_push = base.iter_objects_in_commits({local_ref})
+
+    # Push all objects
+    for oid in objects_to_push:
+        data.push_object(oid, remote_path)
+
+    # Update server ref to our value
+    with data.change_git_dir(remote_path):
+        data.update_ref(refname, data.RefValue(symbolic=False, value=local_ref))
 
 
 def _get_remote_refs(remote_path, prefix=""):
